888c3ef26c87b81890c3735c4ddf29e4291a6db5
tingsx <130417077+tingsx@users.noreply.github.com>
2023-05-06 20:25:58 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -92,58 +92,56 @@ def registerEmp():
 @app.route("/AddEmp", methods=['POST'])
 def AddEmp():
     if request.method == 'POST':
-        try:
-            emp_id = request.form['emp_id']
-            first_name = request.form['first_name']
-            last_name = request.form['last_name']
-            pri_skill = request.form['pri_skill']
-            location = request.form['location']
-            emp_image_file = request.files['emp_image_file']
-        except KeyError as e:
-            return f"Missing required key: {str(e)}", 400
+        emp_id = request.form.get('emp_id')
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        pri_skill = request.form.get('pri_skill')
+        location = request.form.get('location')
+        emp_image_file = request.files.get('emp_image_file')
         
-        insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
-        cursor = db_conn.cursor()
-
-        if emp_image_file.filename == "":
-            return "Please select a file"
-
-        try:
-
-            cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
-            db_conn.commit()
-            emp_name = "" + first_name + " " + last_name
-            # Uplaod image file in S3 #
-            emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
-            s3 = boto3.resource('s3')
-
-            try:
-                print("Data inserted in MySQL RDS... uploading image to S3...")
-                s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-                bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-                s3_location = (bucket_location['LocationConstraint'])
-
-                if s3_location is None:
-                    s3_location = ''
-                else:
-                    s3_location = '-' + s3_location
-
-                object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-                    s3_location,
-                    custombucket,
-                    emp_image_file_name_in_s3)
-
-            except Exception as e:
-                return str(e)
-
-        finally:
-            cursor.close()
-
-        print("all modification done...")
-        return render_template('AddEmpOutput.html', name=emp_name)
-
+        # rest of the code
     else:
         return "Invalid request method"
+
+    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
+    cursor = db_conn.cursor()
+
+    if emp_image_file.filename == "":
+        return "Please select a file"
+
+    try:
+
+        cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
+        db_conn.commit()
+        emp_name = "" + first_name + " " + last_name
+        # Uplaod image file in S3 #
+        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+        s3 = boto3.resource('s3')
+
+        try:
+            print("Data inserted in MySQL RDS... uploading image to S3...")
+            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
+            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location['LocationConstraint'])
+
+            if s3_location is None:
+                s3_location = ''
+            else:
+                s3_location = '-' + s3_location
+
+            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                s3_location,
+                custombucket,
+                emp_image_file_name_in_s3)
+
+        except Exception as e:
+            return str(e)
+
+    finally:
+        cursor.close()
+
+    print("all modification done...")
+    return render_template('AddEmpOutput.html', name=emp_name)
 
 
 
