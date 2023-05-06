1a1213878e8c7ce95a92506ac5a119377aac5151
tingsx <130417077+tingsx@users.noreply.github.com>
2023-05-06 11:28:44 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 ycustomhost = "ting-employee.cpxrtodkrhyv.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Bait3273"
+custompass = "12345678"
 customdb = "employee"
 custombucket = "tingshuxuan-bucket"
 customregion = "us-east-1"
