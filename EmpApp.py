abf73a5c84fd7c12fd44aca05b8b9482c199460d
tingsx <130417077+tingsx@users.noreply.github.com>
2023-05-07 17:03:42 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -228,9 +228,7 @@ def CheckIn():
             insert_sql = "INSERT INTO Attendance (emp_id) VALUES (%s)"
             cursor = db_conn.cursor()
     
-            CheckInTime = datetime.now()
-            formatted_login = CheckInTime.strftime('%d/%m/%Y %H:%M:%S')
-            print("Check In Time: {}", formatted_login)
+            
                                            
             try:
                 cursor.execute(update_statement, {'check_in' : formatted_login, 'emp_id':int(emp_id)})
@@ -244,6 +242,13 @@ def CheckIn():
             return render_template("/CheckIn", date = datetime.now(), CheckInTime = formatted_login)
         else:
             return render_template('CheckIn.html')
+            insert_sql = "INSERT INTO Attendance (check_in) VALUES (%s)"
+            cursor = db_conn.cursor()
+            
+            CheckInTime = datetime.now()
+            formatted_login = CheckInTime.strftime('%d/%m/%Y %H:%M:%S')
+            print("Check In Time: {}", formatted_login)
+            
     else:
         return render_template('CheckIn.html')
                              
